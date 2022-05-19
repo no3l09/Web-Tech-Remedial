4a3d19c23fba48f91b7f04762aeb1f399ed0c0d3
no3l09 <90023845+no3l09@users.noreply.github.com>
2022-05-19 16:26:47 +0530

    updated the var count from 0 to 2501
    Initially was getting only 1 output/card when I clicked generate at default state.

--- Web_Tech_Remedial_NoelPhilip(Updated)/main.js
+++ Web_Tech_Remedial_NoelPhilip(Updated)/main.js
@@ -104,7 +104,7 @@ function sortAgedec(){
 var slider = document.getElementById("slider");
 var output = document.getElementById("value");
 output.innerHTML = slider.value; // Display the default slider value
-var count = 0;
+var count = 2501;
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
   output.innerHTML = this.value;
